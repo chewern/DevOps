cde6239abbf69b4d861d252791d63a82ad642897
CHEWERN <CHEWERN@gmail.com>
2021-04-21 09:13:15 +0800

    Trial trigger

--- backend/baojiak.js
+++ backend/baojiak.js
@@ -18,4 +18,18 @@ router.get("/baojia2", (request, response) => {
     );
 });
 
+router.get("/", (request, response) => {
+    database.connection.query(
+        `select * from baojia_2`,
+        (errors, results) => {
+            if (errors) {
+                console.log(errors);
+                response.status(500).send("Internal Server Error");
+            } else {
+                response.status(200).send(results);
+            }
+        }
+    );
+});
+
 module.exports = { router };
